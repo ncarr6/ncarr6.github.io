763801730b483d5bfad5c977376e2946a173fd25
ncarr6 <142940383+ncarr6@users.noreply.github.com>
2023-10-22 20:52:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const textToType = "3X certified Salesforce professional and Army veteran with an active security clearance. Dedicated to efficiency and delivering top-tier service. Reputation for reliability, proactive problem-solving, precision, and an eagerness for continuous learning. Proficient in data management; excels in designing and configuring Salesforce solutions, streamlining processes, and monitoring key metrics.";
 
+const typingElement = document.getElementById('typing-effect');
+let charIndex = 0;
+
+function typeText() {
+    if (charIndex < textToType.length) {
+        typingElement.textContent += textToType.charAt(charIndex);
+        charIndex++;
+        setTimeout(typeText, 40); 
+    }
+}
+
+typeText();
